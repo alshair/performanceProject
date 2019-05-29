b18287b3ae7d61996ff88e6e6101f697c4795f92
Mohamed saad <fireskull111196@gmail.com>
2019-05-29 03:22:25 +0200

    Update performanceProject.m

--- performanceProject.m
+++ performanceProject.m
@@ -1,1 +1,15 @@
-hi
+%text = fileread('C:\Users\Mohamed Elshek\Desktop\log.txt');
+%reads a file and turn it to string
+
+opts = detectImportOptions('log.txt','Delimiter','.');
+
+t = readtable('log.txt',opts);
+
+time = t(:,2);
+commands = t(:,1);
+
+t
+time
+commands
+
+
