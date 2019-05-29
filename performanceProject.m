fbb2975874a0d062d2e9966c12830ddc470b0078
Mohamed saad <fireskull111196@gmail.com>
2019-05-29 09:21:36 +0200

    a5er ta3deel weseltelo

--- performanceProject.m
+++ performanceProject.m
@@ -5,13 +5,49 @@ opts = detectImportOptions('log.txt','Delimiter','.');
 
 t = readtable('log.txt',opts);
 
-time = t(:,2);
+time = t{:,2};
 commands = t{:,1};
 
 t;
-time;
-commands;
-commands = categorical(commands);
 
+commands = categorical(commands);
+time = categorical(time);
+
+callOfa = 0;
+callOfb = 0;
+callOfc = 0;
+    
+if(commands(1) == 'Start/callmain')
+for i = 1:height(t)
+   
+ 
+    if (commands(i)=='Call_a') 
+      
+      callOfa = callOfa + 1;
+      a.name='event a';
+      a.calledTimes= callOfa;
+      
+    elseif (commands(i)=='Call_b')
+       
+         callOfb = callOfb + 1;
+          b.name='event b';
+      b.calledTimes= callOfb;
+     
+    elseif (commands(i)=='Call_c')
+     
+         callOfc = callOfc + 1;
+          c.name='event c';
+      c.calledTimes= callOfc;
+      
+         
+   
+    end
+
+end
+end
+
+a
+b
+c
 
 
